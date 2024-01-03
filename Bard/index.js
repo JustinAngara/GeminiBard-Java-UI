b15411a484b7baac433fae82febfdc6e8bb7f86b
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-01-03 01:13:05 -0600

    Update index.js

--- Bard/index.js
+++ Bard/index.js
@@ -18,31 +18,4 @@ console.log(
 	})
 );
 
-// import './node_modules/cross-fetch/dist/node-polyfill.js';
-// import fs from "fs";
-// import Gemini from "gemini-ai";
-
-// const gemini = new Gemini(`AIzaSyAwu6kKeryvX9DrFlYA9499YhuIzD6QiIw`);
-
-// console.log(
-// 	gemini.ask("What's this show?", {
-// 		data: [fs.readFileSync("./please work.png")],
-// 	})
-// );
-
-
-
-// import { MongoClient } from 'mongodb'
-// var url = "mongodb+srv://justinangara:BYQdys8HoSgz4PfH@mydatabase.kzsr5i0.mongodb.net/";
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("bookstore");
-//   //Find the first document in the customers collection:
-//   dbo.collection("books").findOne({}, function(err, result) {
-//     if (err) throw err;
-//     console.log(result.name);
-//     db.close();
-//   });
-// });
 
